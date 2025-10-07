666323a9ee6a8b2afa27c9f01ed2f44c605fb9e5
Ashish Rana <ashishrana288@gmail.com>
2025-10-07 12:19:02 +0530

    add login signup code

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const express = require('express');
+const mongoose = require('mongoose');
+
+const userRoute = require('./routes/user');
 const app = express();
 
 const PORT = 8021;
 
+mongoose.connect('mongodb://localhost:27017/weblog').then(e => console.log("MongoDB connected")).catch(err => console.log(err));
 
 
 app.set('view engine', 'ejs');
@@ -12,6 +16,8 @@ app.set("views", path.resolve("./views"));
 app.get('/', (req, res) => {
     res.render('home');
 });
+app.use(express.urlencoded({extended: false}));
 
+app.use('/user', userRoute);
 
 app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
